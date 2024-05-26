e7a37ab13d63be289f896676a9572d3854bc88ad
Himank Dave <himank.dave40@gmail.com>
2024-05-26 13:34:03 -0400

    removed vulnerabilities

--- project_blog/settings.py
+++ project_blog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jake.peralta0400@gmail.com'
-EMAIL_HOST_PASSWORD = 'mgbtbxbkjvgsxgmy'
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('PWD')
